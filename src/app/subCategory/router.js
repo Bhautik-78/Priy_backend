222995823a253f0ec0b274839d3c71a722b0cd9f
Bhautik <bhautik211298@gmail.com>
2023-09-18 14:05:44 +0530

    change api method

--- src/app/subCategory/router.js
+++ src/app/subCategory/router.js
@@ -18,7 +18,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 router.post("/v1/", upload.single("file"), controller.createSubCategory);
-router.post("/v1/", controller.getAllSubCategory);
+router.post("/v1/getAllCategory", controller.getAllSubCategory);
 router.put("/v1/:id",upload.single("file"), controller.editSubCategory);
 router.post("/:id", controller.getSubCategory);
 router.delete("/v1/:id", controller.deleteSubCategory);
